5e034d8379f99529435adb29c1e03980e16ac3dd
Mohammad Yasin <mohammad.yasin@students.makeschool.com>
2021-02-07 17:51:17 -0700

    Created the blueprint and added deposit and withdraw method.

--- BankAccount.py
+++ BankAccount.py
@@ -1,1 +1,14 @@
-# Hello there!
+class BankAccount:
+    def __init__(self, full_name, account_number, routing_number, balance = 0):
+        self.full_name = full_name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return f'Amount Deposited:  ${self.balance}'
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return f'Amount Withdrawn:  ${self.balance}'
