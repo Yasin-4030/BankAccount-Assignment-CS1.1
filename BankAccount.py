baf057276e35198f45eeb733f667d6abf14c2e0f
Mohammad Yasin <mohammad.yasin@students.makeschool.com>
2021-02-08 01:03:13 -0700

    More methods and creating the objects.

--- BankAccount.py
+++ BankAccount.py
@@ -6,9 +6,39 @@ class BankAccount:
         self.balance = balance
 
     def deposit(self, amount):
-        self.balance += amount
-        return f'Amount Deposited:  ${self.balance}'
+        self.balance = self.balance + amount
+        print(f'Amount Deposited:  ${amount}')
+        print(f'Current balance is:  ${self.balance}')
 
     def withdraw(self, amount):
-        self.balance -= amount
-        return f'Amount Withdrawn:  ${self.balance}'
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f'Amount Withdrawn:  ${amount}')
+            print(f'Current balance is:  ${self.balance}')
+        else:
+            self.balance = self.balance - 10
+            print(f'Insufficient founds!')
+            print(f'You have been charged $10 for this attempt, your balance is ${self.balance} now.')
+
+    def get_balance(self):
+        print(f'Hi {self.full_name}! Your current balance is:  ${self.balance}')
+
+    def add_interest(self):
+        monthly_interest = self.balance * 0.00083
+        print(f'Monthly interest on your account is ${monthly_interest}, your current balance is: ${self.balance + monthly_interest}')
+
+    def ATM_withdraw(self, amount):
+        self.balance = self.balance - amount - 2
+        print(f'Amount Withdrawn: ${amount}')
+        print(f'You were charged $2 for using this ATM, your current balance is: ${self.balance}')
+
+    def print_reciept(self):
+        print(user1.__dict__)
+
+
+user1 = BankAccount('Joi Anderson', 9438754975, 4565659880, 8000)
+# user1.withdraw(300)
+# user1.get_balance()
+# user1.add_interest()
+user1.ATM_withdraw(200)
+user1.print_reciept()
