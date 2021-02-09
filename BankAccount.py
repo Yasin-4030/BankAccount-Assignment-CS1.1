65b8f4294cfa8cfef97473c6a7afd4d4e489d352
Mohammad Yasin <mohammad.yasin@students.makeschool.com>
2021-02-08 22:41:33 -0700

    Making some corrections.

--- BankAccount.py
+++ BankAccount.py
@@ -1,8 +1,10 @@
+import random
+
 class BankAccount:
-    def __init__(self, full_name, account_number, routing_number, balance = 0):
+    def __init__(self, full_name, balance = 0):
         self.full_name = full_name
-        self.account_number = account_number
-        self.routing_number = routing_number
+        self.account_number = random.randint(10000000, 99999999)
+        self.routing_number = random.randint(10000000, 99999999)
         self.balance = balance
 
     def deposit(self, amount):
@@ -11,14 +13,14 @@ class BankAccount:
         print(f'Current balance is:  ${self.balance}')
 
     def withdraw(self, amount):
-        if amount <= self.balance:
+        if self.balance >= amount:
             self.balance -= amount
             print(f'Amount Withdrawn:  ${amount}')
             print(f'Current balance is:  ${self.balance}')
         else:
-            self.balance = self.balance - 10
+            self.balance -= amount + 10
             print(f'Insufficient founds!')
-            print(f'You have been charged $10 for this attempt, your balance is ${self.balance} now.')
+            print(f'You have been charged $10 for this transaction, your balance is ${self.balance} now.')
 
     def get_balance(self):
         print(f'Hi {self.full_name}! Your current balance is:  ${self.balance}')
@@ -33,12 +35,14 @@ class BankAccount:
         print(f'You were charged $2 for using this ATM, your current balance is: ${self.balance}')
 
     def print_reciept(self):
-        print(user1.__dict__)
-
-
-user1 = BankAccount('Joi Anderson', 9438754975, 4565659880, 8000)
-# user1.withdraw(300)
+        for key in self.__dict__:
+            # print(f"{key}")
+	        print(f"{key}: {self.__dict__[key]}")
+            
+user1 = BankAccount('Jay Lowe', 1800)
+# user1.deposit(100)
+# user1.withdraw(850) 
 # user1.get_balance()
 # user1.add_interest()
-user1.ATM_withdraw(200)
-user1.print_reciept()
+# user1.ATM_withdraw(200)
+# user1.print_reciept()
